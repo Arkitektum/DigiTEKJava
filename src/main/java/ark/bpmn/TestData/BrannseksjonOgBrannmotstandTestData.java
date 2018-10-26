3c568f6515ba48a46b3d82ca14c3cdd58863254d
Matias Gonzalez Tognoni <matias@arkitektum.no>
2018-10-26 13:56:52 +0200

    Add BrannseksjonOgBrannmotstand

---
+++ src/main/java/ark/bpmn/TestData/BrannseksjonOgBrannmotstandTestData.java
@@ -0,0 +1,81 @@
+package ark.bpmn.TestData;
+
+import java.util.Map;
+
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
+
+public class BrannseksjonOgBrannmotstandTestData {
+	public static final String IntegrationModelKey = "BrannseksjonOgBrannmotstandIntModel";
+	public static final String UserTaskId = "UserBrannseksjonOgBrannmotstandIntModelOutput";
+	public static final String EndTaskId = "EndBrannseksjonOgBrannmotstandIntModel";
+	public static final String ModelKey = "BrannseksjonOgBrannmotstand";
+
+	public class models {
+		public static final String Bpmn_BrannseksjonOgBrannmotstand = "./Bpmn/Brannseksjon og Brannmotstand Model.bpmn";
+	    public static final String BpmnInt_BrannseksjonOgBrannmotstand = "BrannseksjonOgBrannmotstandIntModel.bpmn";
+	    public static final String Dmn_03_TiltakStorrelseBrannseksjonBelastning = "./Dmn/03_TiltakStorrelseBrannseksjonBelastning.dmn";
+	    public static final String Dmn_04_BrannmotstandSeksjoneringsvegg = "./Dmn/04_ BrannmotstandSeksjoneringsvegg.dmn";
+	    public static final String Dmn_17_BrannmotstandDorISeksjvegg = "./Dmn/17_BrannmotstandDorISeksjvegg.dmn";
+	    public static final String Dmn_07_BrannmotstandSkillendeKonstruksjon = "./Dmn/07_BrannmotstandSkillendeKonstruksjon.dmn";
+	    public static final String Dmn_16_BrannmotstandDorRomningsvei = "./Dmn/16_BrannmotstandDorRomningsvei.dmn";
+	    public static final String Dmn_18_BrannmotstandVinduMotstParallellYttervegg = "./Dmn/18_BrannmotstandVinduMotstParallellYttervegg.dmn";
+	    public static final String Dmn_20_BranncelleRomningUtgang = "./Dmn/20_BranncelleRomningUtgang.dmn";
+
+	}
+
+	public static Map<String, Object> BrannseksjonOgBrannmotstandOpt01() {
+		VariableMap variableMap = Variables.createVariables()
+				.putValue("konsekvensAvBrann", "Middels konsekvens");
+		return variableMap;
+	}
+
+	public static Map<String, Object> BrannseksjonOgBrannmotstandOpt02() {
+		VariableMap variableMap = Variables.createVariables()
+				.putValue("typeVirksomhet", "Bolig")
+				.putValue("brannenergi", 801)
+				.putValue("arealBrannseksjonPrEtasje", 1900)
+				.putValue("rkl", "RKL1")
+				.putValue("bkl", "BKL1");
+		return variableMap;
+	}
+
+	public static Map<String, Object> Dmn_TiltakStorrelseBrannseksjonBelastning() {
+		VariableMap variableMap = Variables.createVariables().putValue("typeVirksomhet", "Pleieinstitusjoner")
+				.putValue("brannenergi", 49).putValue("arealBrannseksjonPrEtasje", 1801);
+		return variableMap;
+	}
+
+	public static Map<String, Object> Dmn_BrannmotstandSeksjoneringsvegg() {
+		VariableMap variableMap = Variables.createVariables().putValue("bkl", "BKL1").putValue("brannenergi", 801);
+		return variableMap;
+	}
+
+	public static Map<String, Object> Dmn_BrannmotstandDorISeksjoneringsvegg() {
+		VariableMap variableMap = Variables.createVariables().putValue("kravBrannmotstSeksjVegg",
+				"REI 180-M A2-s1,d0 [A 180]");
+		return variableMap;
+	}
+
+	public static Map<String, Object> Dmn_BrannmotstandSkillendeKonstruksjon() {
+		VariableMap variableMap = Variables.createVariables().putValue("bkl", "BKL1");
+		return variableMap;
+	}
+
+	public static Map<String, Object> Dmn_BrannmotstandDorRomningsvei() {
+		VariableMap variableMap = Variables.createVariables().putValue("bkl", "BKL3");
+		return variableMap;
+	}
+
+	public static Map<String, Object> Dmn_BrannmotstandMotstaendeVinduerYttervegger() {
+		VariableMap variableMap = Variables.createVariables().putValue("bkl", "BKL2")
+				.putValue("avstandMellomMotstVinduerIMeter", 2);
+		return variableMap;
+	}
+
+	public static Map<String, Object> Dmn_BranncelleRomningUtgang() {
+		VariableMap variableMap = Variables.createVariables().putValue("rkl", "RKL4");
+		return variableMap;
+	}
+
+}
---
+++ src/test/java/ark/bpmn/DigiTEK/BrannseksjonOgBrannmotstandBpmnTests.java
@@ -0,0 +1,67 @@
+package ark.bpmn.DigiTEK;
+
+import static ark.bpmn.TestData.BrannseksjonOgBrannmotstandTestData.*;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.init;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.processEngine;
+
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.assertThat;
+
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import ark.bpmn.TestData.BrannseksjonOgBrannmotstandTestData.models;
+
+
+@Deployment(resources = {models.BpmnInt_BrannseksjonOgBrannmotstand, models.Bpmn_BrannseksjonOgBrannmotstand,models.Dmn_03_TiltakStorrelseBrannseksjonBelastning,models.Dmn_04_BrannmotstandSeksjoneringsvegg,
+		models.Dmn_07_BrannmotstandSkillendeKonstruksjon,models.Dmn_16_BrannmotstandDorRomningsvei,models.Dmn_17_BrannmotstandDorISeksjvegg,models.Dmn_18_BrannmotstandVinduMotstParallellYttervegg,models.Dmn_20_BranncelleRomningUtgang})
+public class BrannseksjonOgBrannmotstandBpmnTests {
+	@ClassRule
+	@Rule
+	public static ProcessEngineRule rule = TestCoverageProcessEngineRuleBuilder.create().build();
+
+	@Before
+	public void setup() {
+		init(rule.getProcessEngine());
+	}
+
+	@Test
+	public void BrannseksjonOgBrannmotstand_bpmnOpt01() {
+
+		ProcessInstance processInstance = processEngine().getRuntimeService().startProcessInstanceByKey(ModelKey,
+				BrannseksjonOgBrannmotstandOpt02());
+
+		assertThat(processInstance).isStarted().isEnded();
+	}
+
+	@Test
+	public void BrannseksjonOgBrannmotstand_bpmnOpt02() {
+
+		ProcessInstance processInstance = processEngine().getRuntimeService().startProcessInstanceByKey(ModelKey,
+				BrannseksjonOgBrannmotstandOpt02());
+
+		assertThat(processInstance).isStarted().isEnded();
+	}
+
+	@Test
+	public void RisikoklasseModel_IntegrationTest() {
+		ProcessInstance processInstance = processEngine().getRuntimeService()
+				.startProcessInstanceByKey(IntegrationModelKey, BrannseksjonOgBrannmotstandOpt02());
+
+		assertThat(processInstance).task(UserTaskId);
+		Task task = rule.getTaskService().createTaskQuery().singleResult();
+		System.out.println(rule.getRuntimeService().getActivityInstance(processInstance.getId()));
+		rule.getTaskService().complete(task.getId());
+
+		// assertThat(processInstance).isStarted().isEnded().hasPassed(IntegrationModelEndTaskId);
+		assertThat(processInstance).isStarted().isEnded().hasPassed(EndTaskId);
+	}
+}
